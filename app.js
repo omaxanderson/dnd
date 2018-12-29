3ba143b3380c92fe8a1b5850b0459b6b93f49b15
O Max Anderson <anderso2@miamioh.edu>
2018-12-29 13:55:50 -0600

    Add routing middleware

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// Login Router
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -14,6 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+app.use('/api/user', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.get('/', (req, res) => {
 	res.send('sup lauren');
