ee734da48a1ff0fbcbb4dd0f3427dadb9a10c372
O Max Anderson <anderso2@miamioh.edu>
2018-12-29 22:44:39 -0600

    Add the post route

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ var router = express.Router();
 // include the controllers here
 var Auth = require('../controllers/login');
 
+// @TODO this needs to be a POST eventually
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	Auth.authorize(req.query)
@@ -17,7 +18,14 @@ router.get('/', function(req, res, next) {
 
 /* POST authorize based on credentials passed */
 router.post('/', (req, res, next) => {
-
+	Auth.authorize(req.query)
+		.then(result => {
+			res.send(result);
+		})
+		.catch(err => {
+			res.status(400);
+			res.send(err);
+		});
 });
 
 module.exports = router;
