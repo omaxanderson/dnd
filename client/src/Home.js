e453733983892b33f7fccde41da4c5d283964ca3
O Max Anderson <anderso2@miamioh.edu>
2019-01-03 22:57:05 -0600

    Add navbar and a list of ideas

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 class Home extends React.Component {
 	render() {
 		return (
-			<button className='btn' onClick={this.test}>Test</button>
+			<React.Fragment>
+			<Navbar />
+			<div className='container'>
+				<button style={{marginTop: '20px'}} className='btn' onClick={this.test}>Test</button>
+				<div style={{height: '1000px'}}>
+					<h3>To Do</h3>
+					<div className='row'>
+						<div className='collection col s4'>
+							<a href='/notes' className='collection-item'>Note taking system</a>
+							<a href='#!' className='collection-item'>Campaign Creator</a>
+							<a href='#!' className='collection-item'>Character Creation</a>
+							<a href='#!' className='collection-item'>Associate characters/users with campaigns</a>
+							<a href='#!' className='collection-item'>Campaign Scheduler</a>
+							<a href='#!' className='collection-item'>Quick name generator</a>
+							<a href='#!' className='collection-item'>Ambient Sound Creator (tavern, weather, etc)</a>
+							<a href='#!' className='collection-item'>Maybe a Spotify integration?</a>
+							<a href='#!' className='collection-item'>Different "modes" for session prep (note taking) vs playing (sound, generators)</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			</React.Fragment>
 		);
 	}
 
