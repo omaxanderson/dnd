ebebf40d9a5ab4a4ef6a2ebd779203164f6f5bb6
O Max Anderson <anderso2@miamioh.edu>
2019-01-03 22:58:45 -0600

    Create custom css file and remove superfluous function from logout component

--- client/src/auth/Logout.js
+++ client/src/auth/Logout.js
@@ -2,16 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 class Logout extends React.Component {
-	/*
-	this.props.auth.logout(() = >{ 
-		return (
-			<Redirect to='/login' />
-		);
-	});
-	*/
-	async componentDidMount() {
-	}
-
+	// @TODO need to send a request to the back end to destroy the session
 	render() {
 		console.log('logging out...');
 		this.props.auth.logout();
