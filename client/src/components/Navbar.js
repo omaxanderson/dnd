de004c16002f3812484d06c2e59d373a165ef61d
O Max Anderson <anderso2@miamioh.edu>
2019-01-27 01:52:50 -0600

    Add a navbar dropdown with the working link for a new note

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
 
 class Navbar extends React.Component {
 
+	componentDidMount() {
+		const elements = document.querySelectorAll('.dropdown-trigger');
+		M.Dropdown.init(elements, {
+			alignment: 'left',
+			coverTrigger: false,
+		});
+	}
+
 	render() {
 		return (
-			<div className='navbar-fixed' style={{marginBottom: '20px'}}>
-				<nav>
-					<div className='nav-wrapper'>
-						<a href='/' className='brand-logo'>Home</a>
-						<ul id='nav-mobile' className='right hide-on-med-and-down'>
-							<li><a href='#'>Item 1</a></li>
-							<li><a href='#'>Item 2</a></li>
-							<li><a href='#'>Item 3</a></li>
-						</ul>
-					</div>
-				</nav>
-			</div>
+			<React.Fragment>
+				<ul id='createDropdown' className='dropdown-content'>
+					<li><Link to='/notes/create'>Note</Link></li>
+					<li><a href='#!'>Campaign</a></li>
+					<li><a href='#!'>Character</a></li>
+					<li><a href='#!'>Item</a></li>
+				</ul>
+				<div className='navbar-fixed' style={{marginBottom: '20px'}}>
+					<nav>
+						<div className='nav-wrapper'>
+							<a href='/' className='brand-logo'>Home</a>
+							<ul id='nav-mobile' className='right hide-on-med-and-down'>
+								<li><a className='dropdown-trigger' href='#!' data-target='createDropdown'>
+									<i className='material-icons'>add</i>
+								</a></li>
+								<li><a href='#!'>Item 1</a></li>
+								<li><a href='#!'>Item 2</a></li>
+								<li><a href='#!'>Item 3</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</React.Fragment>
 		);
 	}
 
