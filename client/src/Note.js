c379016eba3b96ef28f9a0f5a1355a6b9202aa28
O Max Anderson <anderso2@miamioh.edu>
2019-01-27 14:53:20 -0600

    Explicitly set the css styling for the nested lists

--- client/src/Note.js
+++ client/src/Note.js
@@ -325,7 +325,8 @@ const styleMap = {
 
 function getBlockStyle(block) {
 	switch (block.getType()) {
-		case 'blockquote': return 'RichEditor-blockquote';
+		case 'blockquote': 
+			return 'RichEditor-blockquote';
 		default: return null;
 	}
 }
