974b216e5a2e69ececc27649c41e1827617784d9
O Max Anderson <omaxwellanderson@gmail.com>
2019-03-14 19:49:27 -0500

    Reconfigure the campaigns api call

--- controllers/campaign.js
+++ controllers/campaign.js
@@ -1,15 +1,32 @@
 const db = require('../database/db');
 
 async function index(userId) {
-	const sql = db.format(`SELECT *
+	const fields = [
+		{
+			field: 'campaign_id',
+			displayName: 'Campaign ID',
+		},
+		{
+			field: 'title',
+			displayName: 'Title',
+		},
+		{
+			field: 'created_at',
+			displayName: 'Created',
+		},
+	];
+
+	const sql = db.format(`SELECT ${fields.map(item => item.field).join(',')}
 		FROM campaign
 		WHERE user_id = ?`, [userId]);
 	console.log(sql);
+
 
 	const campaigns = await db.query(sql);
 	return JSON.stringify({
 		metadata: {
 			numResults: campaigns.length,
+			fieldNames: fields,
 		},
 		campaigns,
 	});
